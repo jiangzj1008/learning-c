225561e8c6596357f14482d87e6d6a5dfc7e3655
jiangzj <jiangzj1008@outlook.com>
2018-04-13 01:18:52 +0800

    day2

--- demo_4.c
+++ demo_4.c
@@ -18,21 +18,75 @@ auto int i
 
 类型转换
 作为参数输入的变量，应该要转化为函数定义时设置的参数类型，如果没有手动转，编译器会自动进去转化
+
+不定数目参数
+int printf(char* format, ...);
+
+多维数组作为函数的参数
+必须声明数组中每一行有多少个元素
+int array_sort(int array[100][50]) / int array_sort(int array[][50])
 */
 
 int
-test(int num) {
-    num--;
-    printf("num == %i\n", num);
+minimum(int val[10]) {
+    int minVal;
+    int i;
 
-    return num;
+    minVal = val[0];
+    for (i = 1; i < 10; i++) {
+        if (val[i] < minVal) {
+            minVal = val[i];
+        }
+    }
+
+    return minVal;
+}
+
+int
+testMinimum(void) {
+    int scores[10];
+    int i;
+    int minScore;
+
+    for (i = 0; i < 10; ++i) {
+        printf("input number %i : ", i);
+        scanf("%i", &scores[i]);
+    }
+    minScore = minimum(scores);
+    printf("minScore is %i\n", minScore);
+
+    return 0;
+}
+
+void
+changeArray(int array[], int n) {
+    int i;
+
+    // 可以改变指针所指的值
+    for (i = 0; i < n; i++) {
+        array[i] *= 2;
+    }
+
+    // 无法直接改变指针的指向
+    // array = NULL;
+}
+
+int
+testChangeArray(void) {
+    int array[3] = {1, 2, 3};
+    int i;
+
+    changeArray(array, 3);
+    for (i = 0; i < 3; i++) {
+        printf("array[%i] is %i\n", i, array[i]);
+    }
+
+    return 0;
 }
 
 int
 main(void) {
-    int n = 2;
-    n = test(n);
-    printf("num == %i\n", n);
-
+    // testMinimum();
+    testChangeArray();
     return 0;
 }
